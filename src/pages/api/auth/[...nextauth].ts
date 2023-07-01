767c283097abb96809bf19d892e7fb903a06adff
GargDivanshu <divanshu.garg113@gmail.com>
2023-07-01 17:12:18 +0530

    sav

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";import GoogleProvider from "next-auth/providers/google";
-import { authOptions } from "~/server/auth";
+import GoogleProvider from "next-auth/providers/google";
+// import { authOptions } from "~/server/auth";
 
 
 
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,11 +1,22 @@
 import React, {FC} from 'react';
 import Image from 'next/image';
 
+
 const SignIn: FC = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2">
-            <Image src="/images/mercer-logo.png" alt="Mercer Logo" width={200} height={200} />
-            <h1>Sign In</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 max-w-[1100px] mx-auto justify-center min-h-screen max-h-fit">
+            <Image 
+            className="w-full h-full  aspect-auto"
+            src="/auth.jpg" alt="Authentication" width={200} height={200} />
+            <div className="my-8">
+               <h1>
+                Be a part of our Fam!
+               </h1>
+
+               <button className="flex rounded-md ">
+                Sign in with Google
+               </button>
+            </div>
 
         </div>
     )
