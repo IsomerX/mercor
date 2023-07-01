890e51e92d0d817e12fb1ddf91ff74b0ed92c149
GargDivanshu <divanshu.garg113@gmail.com>
2023-07-02 03:09:31 +0530

    save

--- src/pages/create_event.tsx
+++ src/pages/create_event.tsx
@@ -15,7 +15,15 @@ import Link from "next/link"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
 import {useForm} from "react-hook-form"
-
+import Image from "next/image"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../components/ui/dropdown-menu"
 
 
 const CreateEvent: NextPage = () => {
@@ -50,26 +58,93 @@ const CreateEvent: NextPage = () => {
     
     {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+        <div className="flex flex-col">
+        <h1 className="font-primary font-bold text-3xl flex  justify-center">
+          Come and register
+          <div className="mx-3">
+          <Image 
+          src="/event.jpeg"
+          alt="event image"
+          width={100}
+          height={50}
+          className="rounded-full"
+          />
+        </div>
+        </h1>
+
+
+        <h1 className="font-primary font-bold text-3xl flex justify-center">
+        <div className="mx-3">
+          <Image 
+          src="/corpo_event.jpeg"
+          alt="event image"
+          width={100}
+          height={50}
+          className="rounded-full"
+          />
+        </div>
+        with us!
+        </h1>
+        </div>
+
+
         <FormField
           control={form.control}
           name="username"
           render={({ field }) => (
             <FormItem>
+              <div className="flex flex-col md:flex-row justify-between">
+                <div className="w-4/5 mr-3">
               <FormLabel >Title</FormLabel>
               <FormControl>
                 <Input placeholder="Title" {...field} />
               </FormControl>
+              </div>
 
+              <div className="w-4/5 ml-3">
               <FormLabel>Date</FormLabel>
               <FormControl>
                 <Input placeholder="Date" {...field} />
               </FormControl>
+              </div>
 
+              
 
-              <FormDescription>
+              </div>
+              {/* <FormDescription>
                 This is your public display name.
-              </FormDescription>
+              </FormDescription> */}
               <FormMessage />
+
+           <div className="flex flex-col md:flex-row w-full justify-between">
+            <div className="flex flex-col w-4/5">
+            <FormLabel className="my-1">Duration</FormLabel>
+              <DropdownMenu >
+              
+  <DropdownMenuTrigger
+  className="border-[1px] rounded-md text-slate-500 p-2 w-full mr-3"
+  >Duration</DropdownMenuTrigger>
+  
+  <DropdownMenuContent>
+    <DropdownMenuLabel>Duration</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>30 mins</DropdownMenuItem>
+    <DropdownMenuItem>45 mins</DropdownMenuItem>
+    <DropdownMenuItem>60 mins</DropdownMenuItem>
+    <DropdownMenuItem>90 mins</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+</div>
+
+<div className="w-4/5 ml-3">
+              <FormLabel>Price</FormLabel>
+              <FormControl>
+                <Input placeholder="INR 199" {...field} />
+              </FormControl>
+              </div>
+</div>
+
             </FormItem>
           )}
         />
