35546b4cfc5d1b5ce9f7c347dba4314cca81755a
korigamik-hypr <korigamik@gmail.com>
2023-07-01 16:20:05 +0530

    livekit updates

---
+++ src/pages/room.tsx
@@ -0,0 +1,33 @@
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import { useEffect, useState } from "react";
+import { env } from "../env.mjs"
+
+export default () => {
+  const room = "quickstart-room";
+  const name = "quickstart-user"; // TODO: interpolate with user name
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch(`/api/livekit?room=${ room }&username=${ name }`);
+      const data = await resp.json();
+      setToken(data.token);
+    })();
+  }, []);
+
+  if (token === "") {
+    return <div>Getting token...</div>;
+  }
+
+  return (
+    <LiveKitRoom
+      serverUrl={env.NEXT_PUBLIC_LIVEKIT_URL}
+      token={token}
+      connect={true}
+      video={true}
+      audio={true}
+    >
+      <VideoConference />
+    </LiveKitRoom>
+  );
+};
