e6b37b6531f6e98e9fe1bcfd5f103245df59a031
GargDivanshu <divanshu.garg113@gmail.com>
2023-07-01 18:09:58 +0530

    save

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -6,10 +6,16 @@ import {FcGoogle} from 'react-icons/fc'
 
 const SignIn: FC = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-[1320px] mx-auto justify-center min-h-screen max-h-fit">
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-[1100px] mx-auto justify-center min-h-screen max-h-fit">
             
-            <div className="h-full bg-gray-700">
-
+            <div className="h-full ">
+             <Image 
+             src="/auth.jpg"
+             alt="auth"
+             className="floating"
+             width={500}
+             height={500}
+             />
             </div>
             <div className="my-8 mx-auto">
 
@@ -24,7 +30,7 @@ const SignIn: FC = () => {
                 Be a part of our Fam!
                </h1>
 
-               <button className="my-4 flex font-secondary rounded-full outline-1 border-2 border-slate-100 mx-auto text-md p-2 px-4">
+               <button className="my-4 flex font-secondary rounded-full outline-1 border-2 border-slate-300 hover:border-0 mx-auto text-md p-2 px-4 hover:bg-slate-400 hover:text-white">
                 <FcGoogle
                 className="mr-2"
                 fontSize={25}
