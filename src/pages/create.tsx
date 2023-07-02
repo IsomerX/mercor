4ee1b74f671a91050a3b4a8edb0fe4345922b732
isomerx <dhruvbakshi12343@gmail.com>
2023-07-03 01:38:36 +0530

    asd

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -38,7 +38,7 @@ const CreateEvent: NextPage = () => {
       capacity: values.capacity,
       name: values.title,
       price: values.price,
-      beginsAt: values.date,
+      beginsAt: new Date(values.date).toUTCString(),
       duration: values.duration,
     });
     setDone(true);
@@ -48,27 +48,23 @@ const CreateEvent: NextPage = () => {
     <div className="relative">
       <Nav />
 
-      {
-        /*
+      {/*
       <div className="absolute h-48 w-48 rounded-full bg-[#0F172A]/50 top-44 filter blur-2xl"> </div>
       <div className="absolute h-48 w-48 rounded-full bg-[#F0C37C]/50 top-64 left-24 filter blur-2xl mix-blend-multiply"> </div>
       <div className="absolute h-48 w-48 rounded-full bg-[#0F172A]/50 bottom-24 right-4 filter blur-2xl mix-blend-multiply"> </div>
       <div className="absolute h-48 w-48 rounded-full bg-[#F0C37C]/50 bottom-14 right-24 filter blur-2xl"> </div>
-      */
-      }
+      */}
 
-      <div className=" bg-[#0F172A] max-w-[600px] mx-auto my-10 p-4 shadow-lg rounded-md border-[1px] border-slate-100 hover:shadow-[#0F1729] hover:shadow-lg ">
+      <div className=" mx-auto my-10 max-w-[600px] rounded-md border-[1px] border-slate-100 bg-[#0F172A] p-4 shadow-lg hover:shadow-lg hover:shadow-[#0F1729] ">
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="rounded-md p-4 bg-white space-y-8 "
+            className="space-y-8 rounded-md bg-white p-4 "
           >
             <div className="flex flex-col">
-              <h1 className="font-primary font-bold text-3xl flex  justify-center">
+              <h1 className="flex justify-center font-primary text-3xl  font-bold">
                 Come and register
-                <div>
-                  {done && "Thank you! Event Created"}
-                </div>
+                <div>{done && "Thank you! Event Created"}</div>
                 <div className="mx-3">
                   <Image
                     src="/event.jpeg"
@@ -80,7 +76,7 @@ const CreateEvent: NextPage = () => {
                 </div>
               </h1>
 
-              <h1 className="font-primary font-bold text-3xl flex justify-center">
+              <h1 className="flex justify-center font-primary text-3xl font-bold">
                 <div className="mx-3">
                   <Image
                     src="/corpo_event.jpeg"
@@ -94,9 +90,8 @@ const CreateEvent: NextPage = () => {
               </h1>
             </div>
 
-            <div className="w-full mr-3 flex justify-between">
+            <div className="mr-3 flex w-full justify-between">
               <FormField
-              
                 control={form.control}
                 name="title"
                 render={({ field }) => (
@@ -126,75 +121,63 @@ const CreateEvent: NextPage = () => {
                   </FormItem>
                 )}
               />
- </div>
+            </div>
 
-              <div className="flex w-full justify-between">
-                  <FormField
-                    control={form.control}
-                    name="price"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Price</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="number"
-                            placeholder="INR 199"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-              
+            <div className="flex w-full justify-between">
+              <FormField
+                control={form.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input type="number" placeholder="INR 199" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                  <FormField
-                    control={form.control}
-                    name="capacity"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Capacity</FormLabel>
-                        <FormControl>
-                          <Input type="number" placeholder="0" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+              <FormField
+                control={form.control}
+                name="capacity"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Capacity</FormLabel>
+                    <FormControl>
+                      <Input type="number" placeholder="0" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-                <FormField
-                  control={form.control}
-                  name="duration"
-                  render={({ field }) => (
-                    <FormItem
-                    className="flex flex-col"
-                    >
-                      <FormLabel
-                      >Duration</FormLabel>
-                      <FormControl
-                      
-                      className="border-[1px] w-fit border-slate-200 rounded-md p-2"
-                      >
-                        <select {...field}>
-                          <option value="30">30 min</option>
-                          <option value="45">45 min</option>
-                          <option value="60">1 hour</option>
-                          <option value="90">1.5 hour</option>
-                          <option value="120">2 hour</option>
-                        </select>
-                      </FormControl>
-                      <FormDescription>
-                        This is the duration of the event
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit">Submit</Button>
-             
-              <FormMessage />
-            
+            <FormField
+              control={form.control}
+              name="duration"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Duration</FormLabel>
+                  <FormControl className="w-fit rounded-md border-[1px] border-slate-200 p-2">
+                    <select {...field}>
+                      <option value="30">30 min</option>
+                      <option value="45">45 min</option>
+                      <option value="60">1 hour</option>
+                      <option value="90">1.5 hour</option>
+                      <option value="120">2 hour</option>
+                    </select>
+                  </FormControl>
+                  <FormDescription>
+                    This is the duration of the event
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+
+            <FormMessage />
           </form>
         </Form>
       </div>
--- src/server/api/routers/event.ts
+++ src/server/api/routers/event.ts
@@ -26,7 +26,11 @@ export const eventRouter = createTRPCRouter({
         await ctx.prisma.event.create({
           data: {
             name: input.name,
-            beginsAt: new Date(input.beginsAt).toISOString(),
+            beginsAt: new Date(
+              new Date(input.beginsAt).toLocaleString("en-us", {
+                timeZone: "IST",
+              })
+            ).toISOString(),
             capacity: input.capacity,
             price: input.price ?? 0,
             duration: input.duration,
