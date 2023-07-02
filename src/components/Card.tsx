3272bc633c58e0752e2eb7776379548a9601c9cf
isomerx <dhruvbakshi12343@gmail.com>
2023-07-03 00:52:25 +0530

    asd

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,71 +1,107 @@
-import { NextPage } from "next";
-import { CiTimer } from "react-icons/ci";
-import { AiOutlineUser } from "react-icons/ai";
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import type { Event, User } from "@prisma/client";
 import type { api } from "~/utils/api";
+import { useState } from "react";
 
-const Card: NextPage<{
+const CardInfoModal: React.FC<{
   event: Event & { organizer: User };
-  mutation: ReturnType<typeof api.user.enrollUser.useMutation>
-}> = ({ event, mutation }) => {
-
-  const buyTicket = async () => {
-    console.log("Buy Ticket");
-    await mutation.mutateAsync({ eventId: event.id })
-  }
-
-  // TODO Create Modal for buying ticket
-
+  visible: boolean;
+  setVisible: (visible: boolean) => void;
+  mutation: ReturnType<typeof api.user.enrollUser.useMutation>;
+  isPurchased: boolean;
+}> = ({ event, visible, setVisible, mutation, isPurchased }) => {
+  if (!visible) return null;
   return (
-    <div className="rounded-xl shadow-md max-w-[300px] text-white">
-      <h1 className="p-3 text-xl bg-[#1E293B]">
-        {event.name}
-      </h1>
-      <Image
-        src="/corpo_event.jpeg"
-        alt="alt"
-        width={300}
-        height={200}
-        style={{ objectFit: "cover" }}
-      />
-      <div className="flex flex-col bg-white text-black p-3">
-        <div className="py-1 flex">
-          <AiOutlineUser
-            fontSize={25}
-            className="mr-3"
-          />
+    <div
+      className="fixed left-0 top-0 z-30 grid h-screen w-screen place-items-center bg-black/20"
+      onClick={(e) => {
+        e.stopPropagation();
+        setVisible(false);
+      }}
+    >
+      <div
+        className="flex max-h-[80vh] w-[600px] flex-col gap-5 rounded-3xl bg-white p-10"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <img
+          src="/concert.jpeg"
+          className="h-[350px] w-full rounded-3xl object-cover"
+          alt=""
+        />
+        <h3 className="font-primary text-5xl">{event.name}</h3>
+        <div className="font-secondary text-slate-600">
+          <div className="text-xl font-bold text-slate-700">Organised By:</div>
           {event.organizer.name}
         </div>
-        <div className="py-1 flex">
-          <CiTimer
-            fontSize={25}
-            className="mr-3"
-          />
-          {event.beginsAt.toLocaleTimeString()}
+        <div className="font-secondary text-slate-600">
+          <div className="text-xl font-bold text-slate-700">
+            Total Seats Left:
+          </div>
+
+          {event.capacity}
         </div>
+        {isPurchased ? (
+          <h3 className="text-center font-primary text-2xl">
+            Already Enrolled!
+          </h3>
+        ) : (
+          <button
+            className={`${
+              mutation.isLoading ? "bg-slate-600" : "bg-slate-800"
+            } w-full cursor-pointer rounded-2xl bg-slate-800 px-8 py-4 font-primary text-2xl text-white transition-all hover:bg-slate-700 hover:opacity-90`}
+            onClick={() => {
+              mutation.mutate({ eventId: event.id });
+            }}
+            disabled={mutation.isLoading}
+          >
+            {!mutation.isLoading ? `Buy Now at: ${event.price}` : "Processing"}
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
+const Card: React.FC<{
+  event: Event & { organizer: User };
+  mutation: ReturnType<typeof api.user.enrollUser.useMutation>;
+  isPurchased: boolean;
+}> = ({ event, mutation, isPurchased }) => {
+  const [modalVisible, setModalVisible] = useState(false);
 
-        <div className="flex justify-between">
-          <div className="rounded-full px-3 bg-[#1E293B] text-white w-fit my-3">
-            {event.capacity} Seats left!
-          </div>
-          <div className="rounded-full px-3 bg-[#1E293B] text-white w-fit my-3">
-            ₹ {event.price}
-          </div>
-          <div className="rounded-full px-3 bg-[#1E293B] text-white w-fit my-3">
-            {event.duration}min
-          </div>
-        </div>
-
+  return (
+    <>
+      <CardInfoModal
+        event={event}
+        visible={modalVisible}
+        setVisible={setModalVisible}
+        mutation={mutation}
+        isPurchased={isPurchased}
+      />
+      <div
+        className="dark-gradient relative flex h-[300px] cursor-pointer items-end rounded-3xl p-5 text-white transition-all"
+        onClick={() => setModalVisible(!modalVisible)}
+      >
+        <img
+          src="/concert.jpeg"
+          className=" absolute left-0 top-0 -z-10 h-full w-full rounded-3xl object-cover"
+          alt=""
+        />
         {/* Buy Ticket With Good looking button*/}
-        <div className="flex justify-center">
-          <div onClick={() => buyTicket()} className="rounded-full px-3 py-2 bg-[#1E293B] text-white w-fit cursor-pointer hover:bg-[#2E3A59] transition-all">
-            Buy Ticket
+        <div className="w-full">
+          <div className="truncate font-primary text-4xl text-white/40">
+            {event.name}
+          </div>
+          <div className="font-secondary text-xl text-white/60">
+            {new Date(event.beginsAt).toLocaleString("en-us", {
+              weekday: "short",
+              year: "numeric",
+              month: "long",
+            })}
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/server/api/routers/event.ts
+++ src/server/api/routers/event.ts
@@ -10,37 +10,39 @@ export const eventRouter = createTRPCRouter({
     return ctx.prisma.event.findMany({ include: { organizer: true } });
   }),
 
-  createEvent: protectedProcedure.input(
-    z.object({
-      name: z.string(),
-      beginsAt: z.string(),
-      capacity: z.number(),
-      price: z.number().optional(),
-      duration: z.number(),
-    }),
-  ).mutation(async ({ ctx, input }) => {
-    console.log('Event CREATING', input, ctx.session?.user.id)
-    try {
-      await ctx.prisma.event.create({
-        data: {
-          name: input.name,
-          beginsAt: new Date(input.beginsAt).toISOString(),
-          capacity: input.capacity,
-          price: input.price ?? 0,
-          duration: input.duration,
-          organizerID: ctx.session?.user.id,
-        }
+  createEvent: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        beginsAt: z.string(),
+        capacity: z.number(),
+        price: z.number().optional(),
+        duration: z.number(),
       })
-    } catch (e) {
-      console.log('ERROR: ', e);
-    }
-  }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      console.log("Event CREATING", input, ctx.session?.user.id);
+      try {
+        await ctx.prisma.event.create({
+          data: {
+            name: input.name,
+            beginsAt: new Date(input.beginsAt).toISOString(),
+            capacity: input.capacity,
+            price: input.price ?? 0,
+            duration: input.duration,
+            organizerID: ctx.session?.user.id,
+          },
+        });
+      } catch (e) {
+        console.log("ERROR: ", e);
+      }
+    }),
 
-  getEvent: publicProcedure.input(z.object({ id: z.string() })).query(
-    ({ ctx, input }) => {
+  getEvent: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
       return ctx.prisma.event.findFirst({
         where: { id: input.id },
       });
-    },
-  ),
+    }),
 });
